b66256e2ae9c9613a66db8f5b199a42e7fb5cc54
mingikim-giv <mingikim.giv@gmail.com>
2024-03-28 18:13:46 +0900

    Modify: NoticeRun Class(printMenu Method add)

--- noticeboard/src/noticeboard/NoticeRun.java
+++ noticeboard/src/noticeboard/NoticeRun.java
@@ -4,12 +4,23 @@ package noticeboard;
 // ㄴ User CRUD
 // ㄴ Board CRUD
 public class NoticeRun {
-	private static NoticeRun instance = new NoticeRun();
-	public static NoticeRun getInstance() {
-		return instance;
+	private final int JOIN = 1;
+	
+	// printMenu
+	private void printMenu() {
+		System.out.println("[===게시판===]");
+		System.out.println("[1]회원 가입");
+		System.out.println("[2]회원 탈퇴");
+		System.out.println("[3]로그인");
+		System.out.println("[4]로그아웃");
+		System.out.println("[5]글 쓰기");
+		System.out.println("[6]글 삭제");
+		System.out.println("[7]게시판 보기");
+		System.out.println("[8]종료");
+		System.out.println("[=========]");
 	}
 	
 	public void run() {
-		
+		printMenu();
 	}
 }
