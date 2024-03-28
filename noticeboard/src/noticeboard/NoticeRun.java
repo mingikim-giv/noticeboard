84162b1eb58d6a2fe0dd31759e377acd3d6593b5
mingikim-giv <mingikim.giv@gmail.com>
2024-03-28 18:19:53 +0900

    Modify: NoticeRun Class(runMenu Method add)

--- noticeboard/src/noticeboard/NoticeRun.java
+++ noticeboard/src/noticeboard/NoticeRun.java
@@ -3,8 +3,19 @@ package noticeboard;
 // ㄴ Map 활용
 // ㄴ User CRUD
 // ㄴ Board CRUD
+
+import java.util.Scanner;
+
 public class NoticeRun {
+	private Scanner scan = new Scanner(System.in);
 	private final int JOIN = 1;
+	private final int LEAVE = 2;
+	private final int LOGIN = 3;
+	private final int LOGOUT = 4;
+	private final int WRITE = 5;
+	private final int DELETE = 6;
+	private final int VIEW = 7;
+	private final int END = 8;
 	
 	// printMenu
 	private void printMenu() {
@@ -20,7 +31,50 @@ public class NoticeRun {
 		System.out.println("[=========]");
 	}
 	
+	// runMenu
+	private void runMenu(int sel) {
+		if(sel == JOIN) {
+			
+		}
+		else if(sel == LEAVE) {
+			
+		}
+		else if(sel == LOGIN) {
+			
+		}
+		else if(sel == LOGOUT) {
+			
+		}
+		else if(sel == WRITE) {
+			
+		}
+		else if(sel == DELETE) {
+			
+		}
+		else if(sel == VIEW) {
+			
+		}
+		else if(sel == END) {
+			
+		}
+	}
+	
+	// input
+	private int inputNumber(String message) {
+		int number = -1;
+		
+		try {
+			System.out.println(message + ":");
+			String input = scan.next();
+			number = Integer.parseInt(input);
+		} catch (Exception e) {
+			System.err.println("숫자를 입력하세요.");
+		}
+		return number;
+	}
+	
 	public void run() {
 		printMenu();
+		runMenu(inputNumber("메뉴 선택"));
 	}
 }
