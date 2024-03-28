13b5d438f8d2b6e715c77ed840c746df44c1386b
mingikim-giv <mingikim.giv@gmail.com>
2024-03-28 17:30:07 +0900

    Add: comment

--- noticeboard/src/noticeboard/NoticeRun.java
+++ noticeboard/src/noticeboard/NoticeRun.java
@@ -1,5 +1,8 @@
 package noticeboard;
-
+// # 콘솔 게시판 만들기
+// ㄴ Map 활용
+// ㄴ User CRUD
+// ㄴ Board CRUD
 public class NoticeRun {
 	private static NoticeRun instance = new NoticeRun();
 	public static NoticeRun getInstance() {
