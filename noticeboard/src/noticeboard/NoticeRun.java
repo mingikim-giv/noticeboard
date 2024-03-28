3fa5809619d372269047edddcbf93d7a138e8aa8
mingikim-giv <mingikim.giv@gmail.com>
2024-03-28 17:28:08 +0900

    Add: NoticeRun package

---
+++ noticeboard/src/noticeboard/NoticeRun.java
@@ -0,0 +1,12 @@
+package noticeboard;
+
+public class NoticeRun {
+	private static NoticeRun instance = new NoticeRun();
+	public static NoticeRun getInstance() {
+		return instance;
+	}
+	
+	public void run() {
+		
+	}
+}
