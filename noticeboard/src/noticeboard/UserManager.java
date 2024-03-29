38df1514edc3dbb13c616d201b56be3546ad514d
mingikim-giv <mingikim.giv@gmail.com>
2024-03-29 10:38:51 +0900

    Modify: UserManager Class(CRUD, userSize, serarchId Method add)

--- noticeboard/src/noticeboard/UserManager.java
+++ noticeboard/src/noticeboard/UserManager.java
@@ -14,5 +14,42 @@ public class UserManager {
 		return instance;
 	}
 	
+	// searchId
+	public int searchId(String id) {
+		for(int i = 0; i < list.size(); i ++) {
+			User user = list.get(i);
+			if(user.getId().equals(id)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 	
+	// C.
+	public void addUser(String id, String pw) {
+		User user = new User(id, pw);
+		list.add(user);
+	}
+	// R.
+	public User getUser(int idx) {
+		User user = list.get(idx);
+		return user;
+	}
+	// U.
+	public User setUser(int idx, User user) {
+		return list.set(idx, user);
+	}
+	// D.
+	public void removeUser(int idx) {
+		if(idx < 0 || idx >= list.size()) {
+			System.err.println("유효하지 않은 범위입니다.");
+			return;
+		}
+		list.remove(idx);
+	}
+	
+	// userSize
+	public int userSize() {
+		return list.size();
+	}
 }
